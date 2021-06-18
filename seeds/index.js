0efed60aa5832cb013f37b71f398d76d0b51efee
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-06-17 22:14:19 -0700

    added seed files

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const seedUsers = require('./user-seeds');
+const seedImages = require('./image-seeds');
+const seedReferences = require('./reference-seeds');
+const seedLikes = require('./like-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('--------------');
+    await seedUsers();
+    console.log('--------------');
+
+    await seedImages();
+    console.log('--------------');
+
+    await seedReferences();
+    console.log('--------------');
+
+    await seedLikes();
+    console.log('--------------');
+
+    process.exit(0);
+};
+
+seedAll();
