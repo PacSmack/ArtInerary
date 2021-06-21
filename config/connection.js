7ff7ad71729595c86aabd951c361883baf7fa567
VeeMcGee <vmcguire6377@outlook.com>
2021-06-21 12:06:43 -0600

    connct to db

--- config/connection.js
+++ config/connection.js
@@ -8,15 +8,15 @@ const sequelize = process.env.JAWSDB_URL
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306
+        port: 3001
     });
 
 // connect without jaws db
-// const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PW', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: '3306'
-// });
+//const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PW', {
+    // host: 'localhost',
+    // dialect: 'mysql',
+     //port: '3306'
+ //});
 
 
 module.exports = sequelize;
