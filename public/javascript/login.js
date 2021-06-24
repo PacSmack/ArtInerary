40316770af90a1ccea104fe8aa805f737ba5c72d
VeeMcGee <vmcguire6377@outlook.com>
2021-06-23 18:34:53 -0600

    removed an h5 and replace with logo, install bulma, created modal and related functions

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,38 @@
-async function loginFormHandler(event) {
+
+    // Get the modal
+    var modal = document.getElementById("modal");
+    
+    //to open the login/signup modal onclick
+    var btn = document.getElementById("myBtn");
+    
+    // Get the <span> element that closes the modal
+    //var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+      modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}async function loginFormHandler(event) {
     event.preventDefault();
   
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+
+
+    
+
+    
+
   
     if (email && password) {
       const response = await fetch('/api/users/login', {
